7d66208ec3a94fe5e24e9617ce1ff3901813fe87
cmoadne <cmoadne@163.com>
2019-02-25 15:06:03 +0800

    day1_ALT+SHIFT+F

--- day1.py
+++ day1.py
@@ -1,2 +1,4 @@
-print('The quick brown fox', 'jumps over', 'the lazy dog')  #comma will be explained to space in terminal 
-print('100 + 200 = ',100+200)
+# comma will be explained to space in terminal
+print('The quick brown fox', 'jumps over', 'the lazy dog')
+print('100 + 200 = ', 100 + 200)
+print('100 + 300 = ', 100 + 400)
